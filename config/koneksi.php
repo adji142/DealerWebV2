3a055847660f4f779fc401e651afbc2635c3a7d1
adji142 <prasetyoajiw@gmail.com>
2019-04-01 09:22:32 +0700

    add config

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,6 +1,7 @@
 <?php
 //buka koneksi ke engine MySQL
-	$Open = mysqli_connect("localhost","root","hsp123","dealsys");
+	$Open = mysqli_connect("koprasiwanitausahamandiri.com","u6018530_root","admin123","u6018530_dealsys");
+	//mysqli_connect("localhost","root","hsp123","dealsys");
 		if (!$Open){
 		die ("Koneksi ke Engine MySQL Gagal !<br>");
 		}
