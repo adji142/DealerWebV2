fb726faed3b2c9d4e0fc6abac8a97dfd7ba4f022
adji142 <prasetyoajiw@gmail.com>
2019-04-30 18:28:29 +0700

    wq

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,8 +1,8 @@
 <?php
 //buka koneksi ke engine MySQL
-	// $Open = mysqli_connect("koprasiwanitausahamandiri.com","u6018530_root","admin123","u6018530_dealsys");
+	$Open = mysqli_connect("koprasiwanitausahamandiri.com","u6018530_root","admin123","u6018530_dealsys");
 	//mysqli_connect("localhost","root","hsp123","dealsys");
-		$Open = mysqli_connect("localhost","root","lagis3nt0s4","dealsys");
+		// $Open = mysqli_connect("localhost","root","lagis3nt0s4","dealsys");
 		if (!$Open){
 		die ("Koneksi ke Engine MySQL Gagal !<br>");
 		}
